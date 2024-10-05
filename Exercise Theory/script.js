fa2f4703233272f48866e604a3f2c7d432c29d44
Elia Schneider <elyssch@gmail.com>
2024-10-05 14:36:54 +0200

    update exercise theory

---
+++ Exercise Theory/script.js
@@ -0,0 +1,27 @@
+/* Create un form in cui l'utente inserira 3 parole,
+queste parole vanno a popolare un array.
+Dopo aver inserito le tre parole l'utente cliccherà
+sul pulsante "conta" e il programma stampare in console le singole parole
+e la lunghezza delle singole parole
+es:
+ciao
+a
+tutti
+
+parola ciao lunghezza 4
+parola a lunghuezza 1
+parola tutti lunghezza 5 */
+
+
+
+let wordOne = "ciao"
+let wordTow = "a"
+let wordThree = "tutti"
+
+console.log("la frase è : " + wordOne + " " +  wordTow + " " + wordThree)
+
+console.log(" parola "+ wordOne + " lunghezza " + wordOne.length)
+console.log(" parola "+ wordTow + " lunghezza " + wordTow.length)
+console.log(" parola "+ wordThree + " lunghezza " + wordThree.length)
+
+
